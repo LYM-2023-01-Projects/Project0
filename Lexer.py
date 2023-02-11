ad35c71c17e9bbc33efec7abc84cf66011aba20a
Nicole <nv.martinezv1@uniandes.edu.co>
2023-02-10 22:36:24 -0500

    listo

--- Lexer.py
+++ Lexer.py
@@ -1,4 +1,4 @@
-keywords = {"robot_r": "kw ROBOT_R", "vars": "kw VARS", "PROCS": "kw PROCS"}
+keywords = {"robot_r": "kw ROBOT_R", "vars": "kw VARS", "procs": "kw PROCS"}
 
 separators = {"[": "sp [", "]": "sp ]", ":": "sp :", ";": "sp ;", "|": "sp |", ",": "sp ,"}
 
@@ -16,47 +16,64 @@ conditions = {"facing":"cd facing", "canput": "cd canPut", "canpick": "cd canPic
             "canjumpindir": "cd canJumpInDir", "canmovetothe": "cd canMoveToThe", "canjumptothe": "cd canJumpToThe", 
             "not": "cd not"
             }
-data_type = {"int": "dt int",}
 
-identifier = {}
+alphabet = {"a": "al a","b": "al b","c": "al c","d": "al d","e": "al e","f": "al f","g": "al g","h": "al h","i": "al i",
+            "j": "al j","k": "al k","l": "al l","m": "al m","n": "al n","o": "al o","p": "al p","q": "al q","r": "al r",
+            "s": "al s","t": "al t","u": "al u","v": "al v","w": "al w","x": "al x","y": "al y","z":"al z"}
 
+numbers = {"0": "nu 0", "1": "nu 1","2": "nu 2","3": "nu 3","4": "nu 4","5": "nu 5","6": "nu 6","7": "nu 7",
+            "8": "nu 8","9": "nu 9"}
 
+variables = {"var":"vr"}
 
+def lexer (file_name:str)->list:
 
-file_name = input("Type the file's name: ")
+    file = open(file_name)
+    file_r = file.read()
+    program = file_r.split('\n')
+    all_tokens = []
 
-file = open(file_name)
+    for line in program:
+        line = line.lower()
+        line_tokens = line.split(" ")
+        if " " not in line:
+            line_tokens = [line]
 
-file_r = file.read()
+        for token in line_tokens:
+            for separator in separators:
+                replacing = " " + separator + " "
+                token=token.replace(separator, replacing)
+                tokens = token.split(" ")
 
-program = file_r.split('\n')
+        for token in tokens:
+            if token != " ":
+                if token in keywords:
+                    all_tokens.append(keywords[token])
 
-all_tokens = []
+                elif token in separators:
+                    all_tokens.append(separators[token])
 
-for line in program:
-    line = line.lower()
-    
-    line_tokens = line.split(" ") ## what if there're 2 spaces?
-    
-    for token in line_tokens:
-        if token != " ":
-            if token in keywords:
-                all_tokens.append(keywords[token])
+                elif token in commands:
+                    all_tokens.append(commands[token])
 
-            elif token in separators:
-                all_tokens.append(separators[token])
+                elif token in control:
+                    all_tokens.append(control[token])
 
-            elif token in commands:
-                all_tokens.append(commands[token])
+                elif token in conditions:
+                    all_tokens.append(conditions[token])
 
-            elif token in control:
-                all_tokens.append(control[token])
+                else:
+                    agregar=True
+                    if token!=" ":
+                        for t in token:
+                            if t in alphabet or numbers:
+                                agregar=True
+                            else:
+                                agregar=False
 
-            elif token in conditions:
-                all_tokens.append(conditions[token])
+                    if agregar==True:
+                        all_tokens.append(variables["var"])
+                    elif agregar==False:
+                        return "Lexical error"
 
-
-print(all_tokens)
-
-    
-
+    return all_tokens
