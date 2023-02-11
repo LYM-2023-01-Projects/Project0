da10970da84c283efda90e56b8c5390249665d69
o.urregov <o.urregovniandes.edu.co>
2023-02-10 22:28:12 -0500

    f

--- Parser.py
+++ Parser.py
@@ -1,6 +1,3 @@
-
-
-
 def ParseMain(lexemes):
     
     # a program of 5 or more tokens is guaranteed
@@ -16,7 +13,6 @@ def ParseMain(lexemes):
     
     else:
         return ParseStartSymbol(lexemes)
-
 
 def ParseStartSymbol(lexemes):
 
@@ -36,23 +32,13 @@ def ParseStartSymbol(lexemes):
         next_token = NextToken(remaining_lexemes) 
         remaining_lexemes = Remaining_Lexemes(remaining_lexemes)
         
+        
         return Parse_Start_Symbol_Prime(next_token, remaining_lexemes)
         
-
-def NextToken(remaing_lexemes):
-    return remaing_lexemes[0]
-
-
-def Remaining_Lexemes(lexemes):
-    
-    lenght = len(lexemes)
-    return lexemes[1:lenght]
-
-
 def Parse_Start_Symbol_Prime(first_token_remaing_lexemes, remaining_lexemes):
     
     # If the first token of the remaining lexemes is a "[", then a block of instructions will follow 
-    
+
     if first_token_remaing_lexemes == 'sp [':
         
         next_token = NextToken(remaining_lexemes) 
@@ -61,10 +47,10 @@ def Parse_Start_Symbol_Prime(first_token_remaing_lexemes, remaining_lexemes):
         return Parse_Block_of_Instructions(next_token, remaining_lexemes)
 
 
-    # If the first token of the remaining lexemes is the keyword VARS, then a block of instructions will follow 
+    # If the first token of the remaining lexemes is the keyword VARS, then a list of variables and procedure definitions must follow
          
     elif first_token_remaing_lexemes == 'kw VARS':
-        
+
         next_token = NextToken(remaining_lexemes) 
         remaining_lexemes = Remaining_Lexemes(remaining_lexemes)
 
@@ -75,8 +61,95 @@ def Parse_Start_Symbol_Prime(first_token_remaing_lexemes, remaining_lexemes):
     else:
         return "The input is syntactically incorrect"
 
-def Parse_Block_of_Instructions(first_token_remaing_lexemes, remaining_lexemes):
+def Parse_Block_of_Instructions(first_token_remaing_lexemes, remaining_lexemes): ## MISSING
     pass
 
 def Parse_VARS_and_PROCS(first_token_remaing_lexemes, remaining_lexemes):
+    
+    # We check whether the first token after kw VARS be an id
+
+    if (len(first_token_remaing_lexemes) >= 4) and (first_token_remaing_lexemes[0:3] =='id'):
+
+
+        # VARS must end with a semicolon
+
+        if "sp ;" not in remaining_lexemes:
+
+            return "The input is syntactically incorrect"
+        
+        else:
+            pos_of_semicolon = remaining_lexemes.index("sp ;")
+
+            next_token = NextToken(remaining_lexemes) 
+            remaining_lexemes = Remaining_Lexemes(remaining_lexemes)
+
+            # We check if there's only one var, then the procedure definition must follow
+            if next_token == "sp ;":
+                
+                next_token = NextToken(remaining_lexemes) 
+                remaining_lexemes = Remaining_Lexemes(remaining_lexemes)
+
+                # After the colon, the kw PROCs must follow
+
+                if next_token == "kw PROCS":
+                
+                    next_token = NextToken(remaining_lexemes) 
+                    remaining_lexemes = Remaining_Lexemes(remaining_lexemes)
+                
+                    return Parse_Procedure_definition(next_token, remaining_lexemes)
+                
+                # if the kw PROCs doesn't follow after the list of variables, it is an error
+                
+                else:
+                    return "The input is syntactically incorrect"
+        
+            else:
+
+                return Parse_List_VARS(next_token, remaining_lexemes)
+
+    # If the list of VARS is empty, then the input would be syntactically incorrect
+    
+    else:
+        return "The input is syntactically incorrect"
+
+
+
+def Parse_List_VARS(next_token, remaining_lexemes):
+    
+    # we chech if there are more variables, if so we parse again
+    if next_token == 'sp ,':
+        
+        next_token = NextToken(remaining_lexemes) 
+        remaining_lexemes = Remaining_Lexemes(remaining_lexemes)
+        
+        return Parse_List_VARS_Prime()
+    
+
+def Parse_List_VARS_Prime():
+    if next_token == 'sp ,':
+        
+        next_token = NextToken(remaining_lexemes) 
+        remaining_lexemes = Remaining_Lexemes(remaining_lexemes)
+
+        return Parse_VARS_and_PROCS(next_token, remaining_lexemes)
+
+    else:
+        return "The input is syntactically incorrect"
+
+def Parse_Procedure_definition(next_token, remaining_lexemes):
     pass
+
+## HELPER FUNCTIONS
+
+def NextToken(remaing_lexemes):
+    return remaing_lexemes[0]
+
+def Remaining_Lexemes(lexemes):
+    
+    length = len(lexemes)
+    return lexemes[1:length]
+
+
+# input = ["kw ROBOT_R", "kw VARS","sp ;","kw PROCS","sp [","sp ]"]
+
+# print(ParseMain(input))
--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-hol = [1,2,3,5,6]
-print(hol[2:len(hol)])
+# hol = "id "
+# print(hol[0:3])
+
+# def love():
+#     if 1==1:
+#         return "Yes"
+
+#     else:
+#         print("12")
+
+# print(love())
+love = "22 3"
+print(len(love))
